8a7571f0bf7dad07f8b261a94c29d6c232f42b6b
Mikhail4ch <mihamakarevi426@gmail.com>
2023-11-13 13:23:07 +0300

    Counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Container from './components/Container';
+import Counter from './components/Count';
 function App() {
   return (
+    <>
     <div className="App">
      <Container/>
+     <hr/>
+     <Counter/>
     </div>
+    </>
   );
 }
 export default App;
---
+++ src/components/Count.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+const Counter = () => {
+    const [count, setCount] = useState(0);
+  
+    const Increment = () => {
+      setCount(count + 1);
+    };
+  
+    const Decrement = () => {
+      if (count > 0) {
+        setCount(count - 1);
+      }
+    };
+  
+    const Reset = () => {
+      setCount(0);
+    };
+  
+    return (
+      <div>
+        <span> <h2>Counter: {count}</h2> </span>
+        <button onClick={Increment}>+</button>
+        <button onClick={Decrement}>-</button>
+        <button onClick={Reset}>0</button>
+      </div>
+    );
+  };
+export default Counter;   
